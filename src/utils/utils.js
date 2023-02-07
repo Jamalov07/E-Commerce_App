0cfcd63a6d85ffa15e947a136462bac7b7765fd3
Jamalov <Jamalovn07@gmail.com>
2023-02-07 22:18:17 +0500

    add template with dynamic elements

---
+++ src/utils/utils.js
@@ -0,0 +1,7 @@
+const createElement = (tagName, className, content) => {
+    const element = document.createElement(tagName);
+    if (className) element.setAttribute("class", className);
+    if (content) element.innerHTML = content;
+    return element;
+};
+  
